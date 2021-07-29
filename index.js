a0d5bb7da4d06368c452cbbb92998f10565d5095
Paul <87786395+paulsmythev@users.noreply.github.com>
2021-07-29 14:58:04 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 //Paul Smyth 3813ICT Trimenster 2 2021
 var adding = (x, y) => {return x + y};
 
-console.log("hello world " + adding(5, 10));
+console.log("I caused a merge conflict " + adding(5, 10));
