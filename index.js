73a90ec44bbc90671d5dfc7703ba88a9068b8f3f
Paul Smyth <s5012116@student.ad.griffith.edu.au>
2021-07-30 09:53:36 +1000

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Paul Smyth 3813ICT Trimenster 2 2021
 var adding = (x, y) => {return x + y};
 
-var username = "paul";
+var adding2 = (x, y) => {return x + y};
 
 console.log("I caused a merge conflict " + adding(5, 10));
